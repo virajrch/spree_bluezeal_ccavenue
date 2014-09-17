c1807ecebaeb971fcad2c5279014cf2c304cf260
BlueZeal <dev@bluezeal.in>
2014-09-17 20:15:58 +0200

    Change in routes: accepting get and post for callback action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Spree::Core::Engine.routes.draw do
   match '/gateway/:order_id/ccavenue/:payment_method_id' => 'ccavenue#show', :as => :gateway_ccavenue, via: [:get, :post]
-  get '/gateway/ccavenue/:id/callback' => 'ccavenue#callback', :as => :gateway_ccavenue_callback
+  match '/gateway/ccavenue/:id/callback' => 'ccavenue#callback', :as => :gateway_ccavenue_callback, via: [:get, :post]
 end
