1121fb701e446ace8d1e6e7215e7beec024af797
BlueZeal <dev@bluezeal.in>
2014-09-20 22:12:42 +0200

    Changes in app/controllers/spree/admin/payment_methods_controller_decorator.rb

--- app/controllers/spree/admin/payment_methods_controller_decorator.rb
+++ app/controllers/spree/admin/payment_methods_controller_decorator.rb
@@ -9,7 +9,7 @@ Spree::Admin::PaymentMethodsController.class_eval do
     url = Base64.decode64('aHR0cHM6Ly9ibHVlemVhbC5pbi9tb2R1bGVfdmFsaWRhdGUvc3VjY2Vzcy5waHA=')
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
-    request = http.post(uri.path, server_address: request.remote_ip, domain_url: request.env['HTTP_HOST'], module_code: 'CCAVEN_N_SC')
+    request = http.post(uri.path, server_address: @request.remote_ip, domain_url: @request.env['HTTP_HOST'], module_code: 'CCAVEN_N_SC')
     http.request(request)
   end
 end
