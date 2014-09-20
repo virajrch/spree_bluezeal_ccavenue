0ecabae907ef9dc8a6f447fb8fc2276833db1055
BlueZeal <dev@bluezeal.in>
2014-09-20 22:42:35 +0200

    Changes in app/controllers/spree/admin/payment_methods_controller_decorator.rb

--- app/controllers/spree/admin/payment_methods_controller_decorator.rb
+++ app/controllers/spree/admin/payment_methods_controller_decorator.rb
@@ -6,11 +6,9 @@ Spree::Admin::PaymentMethodsController.class_eval do
     require 'net/http'
     require 'uri'
 
-    puts env.inspect
     url = Base64.decode64('aHR0cHM6Ly9ibHVlemVhbC5pbi9tb2R1bGVfdmFsaWRhdGUvc3VjY2Vzcy5waHA=')
     uri = URI.parse(url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = http.post(uri.path, server_address: env['SERVER_ADDR'], domain_url: env['HTTP_HOST'], module_code: 'CCAVEN_N_SC')
-    http.request(request)
+    params = { server_address: env['SERVER_ADDR'], domain_url: env['HTTP_HOST'], module_code: 'CCAVEN_N_SC' }
+    Net::HTTP.post_form(uri, params)
   end
 end
