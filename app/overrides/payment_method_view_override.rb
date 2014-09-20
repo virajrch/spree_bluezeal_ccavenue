584f248b89501e05d0e16bdb0e78a845d9f48f3e
BlueZeal <dev@bluezeal.in>
2014-09-20 21:05:03 +0200

    Admin payment view override via Deface cont.

--- app/overrides/payment_method_view_override.rb
+++ app/overrides/payment_method_view_override.rb
@@ -1,6 +1,12 @@
 Deface::Override.new(:virtual_path => 'spree/admin/payment_methods/_form',
-                     :name => 'Environment and URL section removal',
-                     :remove => "label[for='ccavenue_payment_method_preferred_url']",
-                     :remove => 'input#ccavenue_payment_method_preferred_url',
+                     :name => 'Environment section removal',
                      :remove => "[data-hook='environment']"
+                     )
+Deface::Override.new(:virtual_path => 'spree/admin/payment_methods/_form',
+                     :name => 'URL label removal',
+                     :remove => "label[for='ccavenue_payment_method_preferred_url']",
+                     )
+Deface::Override.new(:virtual_path => 'spree/admin/payment_methods/_form',
+                     :name => 'URL input removal',
+                     :remove => 'input#ccavenue_payment_method_preferred_url',
                      )
